e0a53387241db1506043da9a227041af99e1c322
Riyabhimani <riyabhimani.me@gmail.com>
2026-03-19 10:48:37 +0530

    working backend

--- auth_api/src/database.service.ts
+++ auth_api/src/database.service.ts
@@ -1,16 +1,49 @@
+// import { Injectable } from '@nestjs/common';
+// import * as mysql from 'mysql2/promise'
+
+// @Injectable()
+// export class DatabaseService {
+//   pool = mysql.createPool({
+//     host:"localhost",
+//     user:"root",
+//     password:"Riyabhimani@2005",
+//     database:"meetingManegment"
+//   });
+//   async query(sql:string,values:any[]){
+//     const [rows] = await this.pool.execute(sql,values);
+//     return rows;
+//   }
+// }
+
+
 import { Injectable } from '@nestjs/common';
-import * as mysql from 'mysql2/promise'
+import * as mysql from 'mysql2/promise';
 
 @Injectable()
 export class DatabaseService {
-  pool = mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"Riyabhimani@2005",
-    database:"meetingManegment"
-  });
-  async query(sql:string,values:any[]){
-    const [rows] = await this.pool.execute(sql,values);
+
+  private pool;
+
+  constructor() {
+    this.pool = mysql.createPool({
+      host: 'mysql-2f86ba98-riya-bf95.j.aivencloud.com',
+      port: 24652,
+      user: 'avnadmin',
+      password: 'AVNS_7G0v2qQQYGXJ_quqylk',
+      database: 'defaultdb',
+
+      ssl: {
+        rejectUnauthorized: false
+      },
+
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0
+    });
+  }
+
+  async query(sql: string, params: any[] = []) {
+    const [rows] = await this.pool.execute(sql, params);
     return rows;
   }
 }
